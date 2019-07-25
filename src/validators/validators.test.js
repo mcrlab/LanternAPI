2dd8278f0e680c2ddac4aae08e0552aca1585cd8
James Linnegar <jlinnega@gmail.com>
2019-07-25 13:31:22 +0100

    Colour validation

--- src/validators/validators.test.js
+++ src/validators/validators.test.js
@@ -1,5 +1,6 @@
-import { timeValidator } from './validators';
+import { timeValidator, colorValidator } from './validators';
 import LightDataError from '../exceptions/LightDataError';
+import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
 
 describe("timeValidator", ()=> {
     test('it should return the time if greater than 0', ()=> {
@@ -16,6 +17,18 @@ describe("timeValidator", ()=> {
     test("it should throw an error if the time is greater than 10", () => {
         const t = () => {
             timeValidator(11);
+        };
+        expect(t).toThrow(LightDataError);
+    })
+});
+
+describe("colorValidator", ()=> {
+    test("it should return the colour if valid RGB hex value", () => {
+        expect(colorValidator("BADA55")).toBe("BADA55");
+    });
+    test("it should throw a LightDataError if the colour is invalid", ()=> {
+        const t = () => {
+            colorValidator("BADCOLOR");
         };
         expect(t).toThrow(LightDataError);
     })
