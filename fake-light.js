5cb18e671022b5a64224a99519db82f755eb3dfb
James Linnegar <jlinnega@gmail.com>
2019-07-25 18:27:52 +0100

    Fake light

---
+++ fake-light.js
@@ -0,0 +1,41 @@
+var mqtt = require('mqtt')
+let state = {
+    "id": "light",
+    "color": {
+        "r":0,
+        "g":0,
+        "b":0
+    }
+};
+
+const client  = mqtt.connect('mqtt://localhost:1883', { will: { topic: 'disconnect', payload: 'me' } })
+
+client.on('connect', () => {
+    console.log("connected to broker");
+    client.subscribe('color/light');
+    client.publish('connect', JSON.stringify(state));
+});
+
+client.on('message', (topic, message) => {
+    console.log(topic, message);
+});
+
+client
+    .on('close', function() {
+        console.log('close');
+    });
+client
+    .on('reconnect', function() {
+        console.log('reconnect');
+    });
+client
+    .on('offline', function() {
+        console.log('offline');
+    });
+client
+    .on('error', function(error) {
+        console.log('error', error);
+    });
+
+ 
+
