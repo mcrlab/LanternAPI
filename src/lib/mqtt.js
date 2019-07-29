4d66d6283e72f3fe114329e9660707332c69e15e
James Linnegar <jlinnega@gmail.com>
2019-07-29 22:27:13 +0100

    mqtt auth from env

--- src/lib/mqtt.js
+++ src/lib/mqtt.js
@@ -3,9 +3,14 @@ var mqtt = require('mqtt')
 export default class MQTTBroker {
 
   init(callback) {
-    
-    this.client  = mqtt.connect(`mqtt://${process.env.mqtt}`)
+    var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
+    var auth = (mqtt_url.auth || ':').split(':');
 
+    this.client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
+      username: auth[0],
+      password: auth[1]
+    });
+    
     this.client.on('connect', () => {
       console.log("connected to broker");
       this.client.subscribe('connect');
