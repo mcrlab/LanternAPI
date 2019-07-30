92588db44d0e010c778366bca7fd22df9f7c5077
James Linnegar <jlinnega@gmail.com>
2019-07-30 06:50:45 +0100

    env for port:

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const lightController = new LightingController(new MQTTBroker());
 
 const app    = createApplication(lightController);
 
-app.listen(3001, '0.0.0.0', () => {
+app.listen(process.env.PORT || '3001', '0.0.0.0', () => {
   console.log('received:');
 });
