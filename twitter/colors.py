98ea2b9dacd9a0c831b7bd823875c0d12bac779e
= <=>
2020-10-22 17:10:36 +0100

    removing twitter API to different repo

--- twitter/colors.py
+++
@@ -1,8 +0,0 @@
-color_list = [
-    {"text":"red","hex":"B80000"},
-    {"text":"orange","hex":"DB3E00"},
-    {"text":"yellow","hex":"FCCB00"},
-    {"text":"green","hex":"008B02"},
-    {"text":"blue","hex":"004DCF"},
-    {"text":"purple","hex":"5300EB"}
-]
