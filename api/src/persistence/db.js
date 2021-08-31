a672617bdd8f449c242c096c6e9e9061f3681ea0
= <=>
2021-08-31 13:44:39 +0100

    reject postgres ssl errors for heroku

--- api/src/persistence/db.js
+++ api/src/persistence/db.js
@@ -2,5 +2,8 @@ const {Pool} = require('pg');
 
 module.exports = new Pool({
   max: 10,
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
