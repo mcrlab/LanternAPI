15a487493162b647f06474af7b5398600e0106d6
= <=>
2021-09-16 17:02:06 +0100

    Incorrect column name

--- api/src/persistence/sequence.js
+++ api/src/persistence/sequence.js
@@ -6,7 +6,7 @@ module.exports = {
     try {
       const timestamp = Date.now() / 1000.0;
       const {rows} = await db.query(sql`
-      INSERT INTO sequence ( wait, data, complete, last_updated )
+      INSERT INTO sequence ( wait, data, complete, created )
       VALUES ( ${wait}, ${data}, FALSE, to_timestamp(${timestamp}) )
       RETURNING *;
       `);
@@ -33,7 +33,7 @@ module.exports = {
 
   async next() {
     const {rows} = await db.query(sql`
-    SELECT * FROM sequence WHERE complete=FALSE ORDER BY last_updated LIMIT 1;
+    SELECT * FROM sequence WHERE complete=FALSE ORDER BY created LIMIT 1;
     `);
     return rows[0];
   },
