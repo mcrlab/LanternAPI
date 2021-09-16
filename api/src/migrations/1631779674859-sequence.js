b168334c6a52776b8a17bbf55710049dddde2c13
= <=>
2021-09-16 09:13:12 +0100

    Sequence table for animations

---
+++ api/src/migrations/1631779674859-sequence.js
@@ -0,0 +1,28 @@
+'use strict'
+const db = require('../persistence/db');
+
+module.exports.up = async function (next) {
+  const client = await db.connect();
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS sequence (
+      id SERIAL PRIMARY KEY,
+      wait INT,
+      data JSON,
+      complete BOOLEAN,
+      last_updated TIMESTAMP;
+    );
+  `);
+  await client.release(true);
+  next()
+}
+
+module.exports.down = async function (next) {
+  const client = await db.connect();
+
+  await client.query(`
+    DROP TABLE sequence;
+  `);
+
+  await client.release(true);
+  next()
+}
