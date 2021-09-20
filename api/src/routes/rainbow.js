7704e6eae187e41d618b98fdaa76cba3b8e78424
= <=>
2021-09-20 12:57:43 +0100

    BGR

--- api/src/routes/rainbow.js
+++ api/src/routes/rainbow.js
@@ -28,24 +28,24 @@ function Wheel(WheelPos){
     WheelPos = 255 - WheelPos;
     if(WheelPos < 85) {
       return {
-          "b":parseInt(255 - WheelPos * 3), 
+          "r":parseInt(255 - WheelPos * 3), 
           "g":0, 
-          "r":parseInt(WheelPos * 3)
+          "b":parseInt(WheelPos * 3)
       };
     }
     if(WheelPos < 170) {
       WheelPos -= 85;
       return {
-          "b":0, 
+          "r":0, 
           "g":parseInt(WheelPos * 3), 
-          "r":parseInt(255 - WheelPos * 3)
+          "b":parseInt(255 - WheelPos * 3)
       };
     }
     WheelPos -= 170;
     return {
-        "b":parseInt(WheelPos * 3), 
+        "r":parseInt(WheelPos * 3), 
         "g":parseInt(255 - WheelPos * 3), 
-        "r":0
+        "b":0
     };
 }
 
